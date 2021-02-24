5e5e5c38f4783a67f77f0bd265cef661726168b3
Yuri <liangy3928@gmail.com>
2021-02-24 20:48:06 +0800

    backup original bash config files

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/env bash
 
 
+## backup bashpofile
+[ -f .bashrc ] && mv -v .bashrc bashrc.old
+[ -f .bash_profile ] && mv -v .bash_profile bash_profile.old
+[ -f .bash_aliases ] && mv -v .bash_aliases bash_aliases.old
+[ -f .bash_logout ] && mv -v .bash_logout bash_logout.old
 
 echo "### Starting ###"
 echo "Begin to config"
 folders=(bash vim tmux git)
-for f in ${folders[@]};
+for f in "${folders[@]}";
 do
-  stow -v -R $f -t ~/
+  stow -v -R "$f" -t ~/
 done
 echo "### Ending ###"
 
